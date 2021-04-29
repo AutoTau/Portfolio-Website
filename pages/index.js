283126912f259e64df0680a13d644383397a3bac
AutoTau <zenbenjam@gmail.com>
2021-04-29 05:36:22 -0700

    Added reactstrap for styling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 
 import BaseLayout from "../components/layouts/BaseLayout"
+import { Container, Button } from "reactstrap";
 
 export default function Home() {
   return (
     <BaseLayout>
-      <div>I am the Index page</div>
+      <Container>
+        <Button color="danger">Danger</Button>
+      </Container>
     </BaseLayout>
   )
 }
