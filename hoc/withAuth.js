6b5cf90b09615d29b0546f5ecd78c3e26bd48b71
AutoTau <zenbenjam@gmail.com>
2021-05-08 01:14:13 -0700

    Now using server side rendering when using withAuth HOC.

--- hoc/withAuth.js
+++ hoc/withAuth.js
@@ -1,8 +1,19 @@
-
+import { useUser } from '@auth0/nextjs-auth0';
+import Redirect from '@/components/shared/Redirect';
 
 const withAuth = (Component) => {
     return props => {
-        return <Component {...props}></Component>
+        const { user, error, loading } = useUser();
+
+        if (loading) {
+            return <p>Loading...</p>
+        }
+
+        if (!user) {
+            return <Redirect ssr to="/api/auth/login" />
+        } else {
+            return <Component user={user} loading={loading} {...props}></Component>
+        }
     }
 }
 
--- pages/secret.js
+++ pages/secret.js
@@ -1,32 +1,16 @@
 
 import BaseLayout from "@/components/layouts/BaseLayout"
 import BasePage from "@/components/BasePage";
-import { useUser } from '@auth0/nextjs-auth0';
-import Redirect from '@/components/shared/Redirect';
 import withAuth from '@/hoc/withAuth';
 
-const Secret = ({ title }) => {
-    const { user, error, loading } = useUser();
-
-    if (loading) {
-        return <p>Loading...</p>
-    }
-
-    if (!user) {
-        return <Redirect to="/api/auth/login" />
-    } else {
-        return (
-            <BaseLayout user={user} loading={loading}>
-                <BasePage>
-                    <h1>I am the Secret Page - {title}</h1>
-                </BasePage>
-            </BaseLayout>
-        )
-    }
+const Secret = ({user, loading}) => {
+    return (
+        <BaseLayout user={user} loading={loading}>
+            <BasePage>
+                <h1>I am the Secret Page - Welcome {user.name}</h1>
+            </BasePage>
+        </BaseLayout>
+    )
 }
-
-// // High Order Component - HOC
-// const withAuth = (Component) => props => 
-//     <Component title="Hello World" {...props}></Component>
 
 export default withAuth(Secret);
