1681223d8236b8f82ee6416ec5f0eb5a6b3f5012
AutoTau <zenbenjam@gmail.com>
2021-05-10 05:22:47 -0700

    retrieving portfolio data by id, server side

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,30 +1,27 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
-import axios from "axios";
 import BasePage from "@/components/BasePage";
-import { useGetPostById } from "@/actions";
-import { useRouter } from 'next/router';
 import { useUser } from '@auth0/nextjs-auth0';
+import PortfolioApi from '@/lib/api/portfolios';
 
-const Portfolio = () => {
-    const router = useRouter();
-    const { data: portfolio, error, loading } = useGetPostById(router.query.id);
+const Portfolio = ({ portfolio }) => {
     const { user, error: errorU, loading: loadingU } = useUser();
     return (
         <BaseLayout user={user} loading={loadingU}>
-            <BasePage>
-                {loading && <p>Loading Data...</p>}
-                {error && <div className="alert alert-danger">{error.message}</div>}
-                {portfolio &&
-                    <>
-                        <h1>I am the Portfolio Page</h1>
-                        <h1>{portfolio.title}</h1>
-                        <p>Body: {portfolio.body}</p>
-                        <p>ID: {portfolio.id}</p>
-                    </>
+            <BasePage header="Portfolio Detail">
+                {
+                    JSON.stringify(portfolio)
                 }
             </BasePage>
         </BaseLayout>
     )
 }
+
+export async function getServerSideProps({ query }) {
+    const json = await new PortfolioApi().getById(query.id);
+    const portfolio = json.data;
+
+    return { props: { portfolio } };
+}
+
 
 export default Portfolio
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -2,24 +2,32 @@ import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/router';
 import PortfolioApi from '@/lib/api/portfolios';
 import { Row, Col } from 'reactstrap';
 import PortfolioCard from '@/components/PortfolioCard';
 
 const Portfolios = ({ portfolios }) => {
+    const router = useRouter();
     const { user, error: errorU, loading: loadingU } = useUser();
 
     return (
         <BaseLayout user={user} error={errorU} loading={loadingU}>
             <BasePage
                 header="Portfolios"
-                className="portfolio-page" >
+                className="portfolio-page">
                 <Row>
                     {portfolios.map(portfolio =>
-                        <Col key={portfolio._id} md="4">
+                        <Col
+                            key={portfolio._id}
+                            onClick={() => {
+                                router.push('/portfolios/[id]', `/portfolios/${portfolio._id}`)
+                            }}
+                            md="4">
                             <PortfolioCard portfolio={portfolio} />
                         </Col>
-                    )}
+                    )
+                    }
                 </Row>
             </BasePage>
         </BaseLayout>
@@ -30,7 +38,6 @@ const Portfolios = ({ portfolios }) => {
 // Improves performance of page
 // creates static page with dynamic data
 export async function getStaticProps() {
-
     const json = await new PortfolioApi().getAll();
     const portfolios = json.data;
     return {
