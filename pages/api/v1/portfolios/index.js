f6a1a3b425b95159729f673addca33965aafc013
AutoTau <zenbenjam@gmail.com>
2021-05-15 02:15:30 -0700

    Refactored to extend blogs/portfolios api's to BaseApi

--- pages/api/v1/portfolios/index.js
+++ pages/api/v1/portfolios/index.js
@@ -4,7 +4,7 @@ import { getAccessToken } from '@auth0/nextjs-auth0';
 export default async function createPortfolio(req, res) {
     try {
         const { accessToken } = await getAccessToken(req, res);
-        const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
+        const json = await new PortfolioApi(accessToken).create(req.body);
         return res.json(json.data);
     } catch (e) {
         return res.status(e.status || 422).json(e.response.data);
