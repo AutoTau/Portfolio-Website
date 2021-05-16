62ce048e6ac552ded66102fc1041c900dcffa651
AutoTau <zenbenjam@gmail.com>
2021-05-16 02:01:12 -0700

    Blog detail page complete.

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,16 +1,37 @@
 
 import BaseLayout from "@/components/layouts/BaseLayout"
 import BasePage from "@/components/BasePage";
+import BlogApi from 'lib/api/blogs';
 import { useUser } from '@auth0/nextjs-auth0';
+import { Row, Col } from 'reactstrap';
+import { SlateView } from 'slate-simple-editor';
 
-const BlogDetail = () => {
+const BlogDetail = ({ blog }) => {
     const { user, error, loading } = useUser();
     return (
         <BaseLayout user={user} loading={loading}>
-            <BasePage>
-                <div>I am the Blog Detail Page</div>
+            <BasePage className="slate-container">
+                <Row>
+                    <Col md={{size: 8, offset: 2}}>
+                        <SlateView initialContent={blog.content}/>
+                    </Col>
+                </Row>
             </BasePage>
         </BaseLayout>
     )
 }
+
+export async function getStaticPaths() {
+    const json = await new BlogApi().getAll();
+    const blogs = json.data;
+    const paths = blogs.map(b => ({ params: { slug: b.slug } }));
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+    const json = await new BlogApi().getBySlug(params.slug);
+    return { props: { blog: json.data } }
+}
+
+
 export default BlogDetail;
