d2648c58f32c10bd17cc992204ef905fc1d0be2e
AutoTau <zenbenjam@gmail.com>
2021-05-20 17:31:16 -0700

    started work on portfolio detail page

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -8,14 +8,28 @@ const Portfolio = ({ portfolio }) => {
 
     // TODO: provide stylings improvement
     return (
-        <BaseLayout user={user} loading={loadingU}>
+        <BaseLayout 
+        navClass="transparent"
+        user={user} 
+        loading={loadingU}>
             <BasePage
-                header="Portfolio Detail"
+                noWrapper
+                indexPage
                 title={`${portfolio.title} - Ben Portis`}
                 metaDescription={portfolio.description}>
-                {
-                    JSON.stringify(portfolio)
-                }
+                <div className="portfolio-detail">
+                    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+                        <main role="main" class="inner page-cover">
+                            <h1 class="cover-heading">Title</h1>
+                            <p class="lead dates">dates</p>
+                            <p class="lead info mb-0">jobTitle | company | location</p>
+                            <p class="lead">description</p>
+                            <p class="lead">
+                                <a href="#" class="btn btn-lg btn-secondary">Visit Company</a>
+                            </p>
+                        </main>
+                    </div>
+                </div>
             </BasePage>
         </BaseLayout>
     )
