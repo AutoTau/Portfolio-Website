a18c9637e5bbf37f2762d25183105b5356370910
AutoTau <zenbenjam@gmail.com>
2021-11-14 11:13:22 -0800

    Resume path fix trial

--- pages/resume.js
+++ pages/resume.js
@@ -3,15 +3,18 @@ import BasePage from "@/components/BasePage";
 import { useUser } from '@auth0/nextjs-auth0';
 import { Row, Col } from 'reactstrap'
 
+
+
 const Resume = () => {
-      const { user, loading } = useUser();
+    const { user, loading } = useUser();
+    var resume = "public/images/Benjamin_Portis_Resume_Final.pdf"
     return (
         <BaseLayout user={user} loading={loading}>
             <BasePage title="My Resume - Ben Portis">
                 <Row>
                     <Col md={{size: 8, offset: 2}}>
                         <p align="center">
-                            <iframe src={`${/public/images/Benjamin_Portis_Resume_Final.pdf}#view=fitH`} style={{width: '100%', height: '100%'}} />
+                            <iframe title="Resume" src={`${resume}#view=fitH`} style={{width: '100%', height: '100%'}} />
                         </p>
                     </Col>
                 </Row>
